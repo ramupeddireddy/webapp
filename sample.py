ad1a09aa04167862c40136d460ef688b09d7a682
ramupeddireddy <ramupeddireddy1999@gmail.com>
2020-07-26 20:17:10 +0530

    c1

---
+++ sample.py
@@ -0,0 +1,22 @@
+# Python code to demonstrate working of 
+# add(), sub(), mul() 
+
+# importing operator module 
+
+
+# Initializing variables 
+a = 4
+
+b = 3
+
+# using add() to add two numbers 
+print ("The addition of numbers is :",end=""); 
+print (add(a, b)) 
+
+# using sub() to subtract two numbers 
+print ("The difference of numbers is :",end=""); 
+print (sub(a, b)) 
+
+# using mul() to multiply two numbers 
+print ("The product of numbers is :",end=""); 
+print (mul(a, b)) 
